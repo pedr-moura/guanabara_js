0a50be8de2ed357da79c78b99e2872137e3de624
Pedro Moura <pedrosousavn@gmail.com>
2024-04-17 17:25:58 -0300

    exercicio oito

---
+++ ex008.js
@@ -0,0 +1,10 @@
+var vel = 60
+console.log(`A velocidade do seu carro é de ${vel}Km/h`)
+
+var limite = 50
+//condicao simples
+if(vel > limite ){
+    console.log('Você ultrapassou a velocidade permitida e foi multado')
+}
+
+console.log('Dirija sempre com segurança')
